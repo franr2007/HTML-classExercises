0f23a80a191040e6c1070b8384329d3ee70443ea
fran <frodriguez4@nigul.cide.es>
2026-02-16 16:52:09 +0100

    new activity, js

---
+++ Actividad 3.4_FranRodriguez/js/vanilla.js
@@ -0,0 +1,12 @@
+var botonAnadir= document.getElementById("botonAnadir");
+var nuevaAssignatura;
+var DAM1año = ["Llenguatges","Sistemes","Programació,"+nuevaAssignatura];
+var DAM2año = ["Interficies","entornos","empresas"];
+var fecha;
+var botonBorrar = document.getElementById("botonBorrar");
+var botonBuscar = document.getElementById("botonBuscar");
+
+nuevaAssignatura= prompt("Introduce una nueva asignatura");
+console.log(nuevaAssignatura);
+fecha=new Date();
+
